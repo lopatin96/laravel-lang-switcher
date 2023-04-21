7e49673027e9688aa152b6f758b63135b86ab52b
vladyslavlopatin <ilopatin3@gmail.com>
2023-04-21 22:52:15 +0200

    added props

--- resources/views/lang-switcher/index.blade.php
+++ resources/views/lang-switcher/index.blade.php
@@ -1,4 +1,6 @@
-<x-dropdown align="bottom" width="40">
+@props(['align' => 'bottom'])
+
+<x-dropdown align="{{ $align }}" width="40">
     <x-slot name="trigger">
         <span class="text-gray-500 hover:text-gray-700 font-medium cursor-pointer">
             {{ __('laravel-lang-switcher::langs.Language') }}

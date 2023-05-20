76fbd2efa256bd88edbee3369a8cb6a1c523f737
vladyslavlopatin <ilopatin3@gmail.com>
2023-05-20 18:44:00 +0200

    use Illuminate\Auth\Events\Login;

--- src/Listeners/UpdateUserLocale.php
+++ src/Listeners/UpdateUserLocale.php
@@ -3,11 +3,12 @@
 namespace Atin\LaravelLangSwitcher\Listeners;
 
 use App\Events\Event;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\App;
 
 class UpdateUserLocale
 {
-    public function handle(Event $event)
+    public function handle(Event|Login $event)
     {
         if (auth()->check()) {
             auth()->user()->locale = $event->locale ?? App::getLocale();

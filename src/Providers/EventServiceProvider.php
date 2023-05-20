ad8b4311dad46871c2e797c47e0af797ea91e59b
vladyslavlopatin <ilopatin3@gmail.com>
2023-05-20 15:50:29 +0200

    Registered

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -5,13 +5,17 @@ namespace Atin\LaravelLangSwitcher\Providers;
 use Atin\LaravelLangSwitcher\Events\LocaleWasChanged;
 use Atin\LaravelLangSwitcher\Listeners\UpdateUserLocale;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Auth\Events\Registered;
 
 class EventServiceProvider extends ServiceProvider
 {
     protected $listen = [
         LocaleWasChanged::class => [
             UpdateUserLocale::class,
-        ]
+        ],
+        Registered::class => [
+            UpdateUserLocale::class,
+        ],
     ];
 
     /**

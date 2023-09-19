b28299689e953d26a4f6c3ece2848abb94a627ae
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-09-19 13:10:33 +0200

    language: <selected language>

--- resources/views/lang-switcher/index.blade.php
+++ resources/views/lang-switcher/index.blade.php
@@ -3,7 +3,7 @@
 <x-dropdown align="{{ $align }}" width="40">
     <x-slot name="trigger">
         <span class="text-gray-500 hover:text-gray-700 font-medium cursor-pointer">
-            {{ __('laravel-lang-switcher::langs.Language') }}
+            {{ __('laravel-lang-switcher::langs.Language') }}: {{ config("laravel-lang-switcher.languages." . session()->get('locale')) }}
         </span>
     </x-slot>
 

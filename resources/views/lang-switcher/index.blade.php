4de18caf0e8f1081e1b5c335bee990bd89f772b3
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-09-19 13:21:26 +0200

    $attributes->merge

--- resources/views/lang-switcher/index.blade.php
+++ resources/views/lang-switcher/index.blade.php
@@ -2,7 +2,7 @@
 
 <x-dropdown align="{{ $align }}" width="40">
     <x-slot name="trigger">
-        <span class="text-gray-500 hover:text-gray-700 font-medium cursor-pointer">
+        <span {{ $attributes->merge(['class' => '']) }}>
             {{ __('laravel-lang-switcher::langs.Language') }}: {{ config("laravel-lang-switcher.languages." . session()->get('locale')) }}
         </span>
     </x-slot>

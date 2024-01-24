a66b6fc9b3becc324e45bae93146c09478ec2dde
vladyslavlopatin <ilopatin3@gmail.com>
2024-01-25 00:40:30 +0100

    fixed lang

--- src/Listeners/UpdateUserLocale.php
+++ src/Listeners/UpdateUserLocale.php
@@ -4,14 +4,13 @@ namespace Atin\LaravelLangSwitcher\Listeners;
 
 use Atin\LaravelLangSwitcher\Events\LocaleWasChanged;
 use Illuminate\Auth\Events\Login;
-use Illuminate\Support\Facades\App;
 
 class UpdateUserLocale
 {
     public function handle(LocaleWasChanged|Login $event)
     {
         if (auth()->check()) {
-            auth()->user()->locale = $event->locale ?? App::getLocale();
+            auth()->user()->locale = $event->locale ?? app()->getLocale();
             auth()->user()->save();
         }
     }

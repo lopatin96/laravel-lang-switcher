ab9972374e015454cb67ecbf73d6b29a35e7df2e
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2024-01-26 12:27:48 +0100

    countries

--- config/config.php
+++ config/config.php
@@ -7,6 +7,48 @@ return [
     'languages' => [
         'en' => 'English',
         'ru' => 'русский',
+    ],
+
+    'countries' => [
+        'us' => [
+            'currency' => 'USD',
+        ],
+        'ca' => [
+            'currency' => 'CAD',
+        ],
+        'gb' => [
+            'currency' => 'GBP',
+        ],
+        'se' => [
+            'currency' => 'SEK',
+        ],
+        'de' => [
+            'currency' => 'EUR',
+        ],
+        'es' => [
+            'currency' => 'EUR',
+        ],
+        'pt' => [
+            'currency' => 'EUR',
+        ],
+        'fr' => [
+            'currency' => 'EUR',
+        ],
+        'au' => [
+            'currency' => 'AUD',
+        ],
+        'jp' => [
+            'currency' => 'JPY',
+        ],
+        'in' => [
+            'currency' => 'INR',
+        ],
+        'id' => [
+            'currency' => 'IDR',
+        ],
+        'nl' => [
+            'currency' => 'EUR',
+        ],
     ]
 
 ];

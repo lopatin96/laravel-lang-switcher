cbd9238a3f7f8a46e31b7f68fb2e62ebec7743b0
vladyslavlopatin <ilopatin3@gmail.com>
2024-03-26 20:48:19 +0100

    fixed LangSwitcher

--- src/Http/Middleware/LangSwitcher.php
+++ src/Http/Middleware/LangSwitcher.php
@@ -10,17 +10,22 @@ class LangSwitcher
 {
     public function handle(Request $request, Closure $next): mixed
     {
+        if (auth()->check()) {
+            app()->setLocale(auth()->user()->locale);
+
+            return $next($request);
+        }
+
         foreach ([
             $request->input('locale'),
             $request->input('country') ? config('laravel-lang-switcher.countries_to_locales')[$request->input('country')] ?? null : null,
             request()->cookie('locale'),
-            auth()->user()?->locale ?? null,
             substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2) ?? null,
             config('app.locale', 'en'),
-        ] as $lang) {
-            if (array_key_exists($lang, config('laravel-lang-switcher.languages'))) {
-                cookie()->queue(cookie('locale', $lang, config('laravel-lang-switcher.cookie_life_in_minutes', 43200)));
-                app()->setLocale($lang);
+        ] as $locale) {
+            if (array_key_exists($locale, config('laravel-lang-switcher.languages'))) {
+                cookie()->queue(cookie('locale', $locale, config('laravel-lang-switcher.cookie_life_in_minutes', 43200)));
+                app()->setLocale($locale);
                 break;
             }
         }

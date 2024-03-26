65dadbb643394918cde72151d6c4e0fc9947a38f
vladyslavlopatin <ilopatin3@gmail.com>
2024-03-26 23:02:35 +0100

    new strategy of getLocale()

--- src/Http/Middleware/LangSwitcher.php
+++ src/Http/Middleware/LangSwitcher.php
@@ -4,32 +4,38 @@ namespace Atin\LaravelLangSwitcher\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
 
 class LangSwitcher
 {
     public function handle(Request $request, Closure $next): mixed
     {
         if (auth()->check()) {
-            app()->setLocale(auth()->user()->locale);
+            app()->setLocale(auth()->user()->locale ?? $this->getLocale($request));
 
             return $next($request);
         }
 
-        foreach ([
+        $locale = $this->getLocale($request);
+
+        cookie()->queue(cookie('locale', $locale, config('laravel-lang-switcher.cookie_life_in_minutes', 43200)));
+        app()->setLocale($locale);
+
+        return $next($request);
+    }
+
+    private function getLocale(Request $request): string
+    {
+        foreach([
             $request->input('locale'),
             $request->input('country') ? config('laravel-lang-switcher.countries_to_locales')[$request->input('country')] ?? null : null,
-            request()->cookie('locale'),
-            substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2) ?? null,
-            config('app.locale', 'en'),
+            $request->cookie('locale'),
+            substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2),
         ] as $locale) {
             if (array_key_exists($locale, config('laravel-lang-switcher.languages'))) {
-                cookie()->queue(cookie('locale', $locale, config('laravel-lang-switcher.cookie_life_in_minutes', 43200)));
-                app()->setLocale($locale);
-                break;
+                return $locale;
             }
         }
 
-        return $next($request);
+        return config('app.locale', 'en');
     }
 }

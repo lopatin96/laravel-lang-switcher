6b85db746f42c203ba5e42ca07204dc2cf51eec2
Vladyslav Lopatin <lopatin@wskz.pl>
2024-07-10 15:14:22 +0200

    add change language

--- resources/views/components/lang-switcher.blade.php
+++ resources/views/components/lang-switcher.blade.php
@@ -2,9 +2,13 @@
 
 <x-dropdown align="{{ $align }}" width="40">
     <x-slot name="trigger">
-        <span {{ $attributes->merge(['class' => '']) }}>
-            {{ __('laravel-lang-switcher::langs.Language') }}: {{ config("laravel-lang-switcher.languages." . app()->getLocale()) }}
-        </span>
+        <div class="flex flex-col leading-tight cursor-pointer">
+            <span class="text-xs font-mono opacity-75">change language</span>
+
+            <span {{ $attributes->merge(['class' => '']) }}>
+                {{ __('laravel-lang-switcher::langs.Language') }}: {{ config("laravel-lang-switcher.languages." . app()->getLocale()) }}
+            </span>
+        </div>
     </x-slot>
 
     <x-slot name="content">
@@ -14,9 +18,9 @@
 
         @foreach(config('laravel-lang-switcher.languages') as $locale => $language)
             <x-dropdown-link
-                :href="route('locale', ['locale' => $locale])"
-                rel="nofollow"
-                class="cursor-pointer {{ $locale === app()->getLocale() ? 'bg-gray-100 font-semibold' : null }}"
+                    :href="route('locale', ['locale' => $locale])"
+                    rel="nofollow"
+                    class="cursor-pointer {{ $locale === app()->getLocale() ? 'bg-gray-100 font-semibold' : null }}"
             >
                 {{ config("laravel-lang-switcher.languages.$locale") }}
             </x-dropdown-link>
